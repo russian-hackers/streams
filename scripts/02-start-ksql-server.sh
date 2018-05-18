41d6bd5a7deca1dc529aec1703694abbadb6dd98
Viktor Gamov <viktor@confluent.io>
2018-05-18 10:11:22 -0700

    ⤴️ updated version of containers
    Signed-off-by: Viktor Gamov <viktor@confluent.io>

--- scripts/02-start-ksql-server.sh
+++ scripts/02-start-ksql-server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker exec russianhackersstreams_ksql-cli_1 ksql-server-start /tmp/ksql.properties >/tmp/ksql.log 2>&1 &
+docker exec russian-hackers-streams_ksql-cli_1 ksql-server-start /tmp/ksql.properties >/tmp/ksql.log 2>&1 &
 sleep 10
-docker-compose exec ksql-cli ksql-cli remote http://localhost:9098 --exec "run script '/tmp/ksqlcommands';"
-
+docker exec russian-hackers-streams_ksql-cli_1 bash -c "ksql http://localhost:9098 <<EOF
+run script '/tmp/ksqlcommands';
+exit ;
+EOF
+"
--- scripts/consume-bintray-feed.sh
+++ scripts/consume-bintray-feed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec russianhackersstreams_connect_1 kafka-console-consumer --bootstrap-server kafka1:9091 --consumer.config=/usr/share/consumer.properties --from-beginning --topic bintray | jq '.payload'
+docker exec russian-hackers-streams_connect_1 kafka-console-consumer --bootstrap-server kafka1:9091 --consumer.config=/usr/share/consumer.properties --from-beginning --topic bintray | jq '.payload'
